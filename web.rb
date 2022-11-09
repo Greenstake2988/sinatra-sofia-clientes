c6581df5062590fbfdb583bc1c23103bce26caa1
Oscar Vazquez <0skr.vazquez@gmail.com>
2022-11-08 22:01:12 -0600

    Mejorando comentarios

--- web.rb
+++ web.rb
@@ -4,22 +4,18 @@ require "sequel"
 # Conexion base de datos
 DB = Sequel.connect('sqlite://cartera.db')
 
-# Routes
+# Ruta raiz
 get '/' do
   redirect '/clientes'
 end
 
+# Ruta para mostrar TODOS los clientes
 get '/clientes' do
   c = DB[:clientes].all
   erb :clientes, :locals => {:clientes => c}
 end
 
-
-get '/clientes/:nombre' do |n|
-  t = DB[:transacciones].where(nombre_cliente: n).all
-  erb :cliente, :locals => {:nombre => n, :transacciones => t}
-end
-
+# Ruta para AGREGAR un cliente
 post '/clientes' do
   # Evitando guardar mayusculas en la base de datos
   unless DB[:clientes].include?(nombre: params[:nombre].downcase)
@@ -28,10 +24,18 @@ post '/clientes' do
     redirect '/'
 end
 
+# Ruta para BORRAR el cliente
 delete '/clientes/:name' do |n|
-  #Hay que borrar primero los childs luego
-  #borramos al padre
+  # Hay que borrar primero los childs luego
+  # borramos al padre
   DB[:transacciones].where(nombre_cliente: n).delete
   DB[:clientes].where(nombre: n).delete
   redirect '/'
+end
+
+# Ruta para mostrar UN cliente
+get '/clientes/:nombre' do |n|
+  # Buscamos las transacciones que pertenecen al cliente
+  t = DB[:transacciones].where(nombre_cliente: n).all
+  erb :cliente, :locals => {:nombre => n, :transacciones => t}
 end
