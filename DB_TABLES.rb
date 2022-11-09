fbf4a471061309c3e2d84078cc155a70b6cc67ed
Oscar Vazquez <0skr.vazquez@gmail.com>
2022-11-08 22:04:33 -0600

    Borrando nuevo_cliente.erb y cambiando nombre DB_test.rb

--- DB_TABLES.rb
+++ DB_TABLES.rb
@@ -2,16 +2,16 @@ require "sequel"
 
 DB = Sequel.connect('sqlite://cartera.db')
 
-#DB.create_table :clientes do
-#    String :nombre, unique: true, null: false, primary_key: true
-#end 
+DB.create_table :clientes do
+    String :nombre, unique: true, null: false, primary_key: true
+end 
 
-#DB.create_table :transacciones do
-#    primary_key :id
-#    Float :monto, null: false
-#    Date  :fecha, null: false
-#    foreign_key :nombre_cliente, :clientes
-#end
+DB.create_table :transacciones do
+    primary_key :id
+    Float :monto, null: false
+    Date  :fecha, null: false
+    foreign_key :nombre_cliente, :clientes
+end
 
 #transacciones = DB[:transacciones]
 #clientes = DB[:clientes]
