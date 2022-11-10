c3c4bd489d31657bb160b007784056f6f4ab8ae6
Oscar Vazquez <0skr.vazquez@gmail.com>
2022-11-10 16:32:30 -0600

    Agregando Relaciones en la base de datos

--- cartera.rb
+++ cartera.rb
@@ -2,34 +2,27 @@ require "sequel"
 
 DB = Sequel.connect('sqlite://cartera.db')
 
-
 #DB.create_table :clientes do
-#    primary_key :id 
-#    String :nombre, unique: true, null: false
+#    String :nombre, unique: true, null: false, primary_key: true
 #end 
 
 #DB.create_table :transacciones do
 #    primary_key :id
 #    Float :monto, null: false
 #    Date  :fecha, null: false
+#    foreign_key :nombre_cliente, :clientes
 #end
 
-
-
-
-#clientes = DB[:clientes]
 #transacciones = DB[:transacciones]
+#clientes = DB[:clientes]
 
 #clientes.insert(nombre: 'oscar')
 #clientes.insert(nombre: 'sofia')
 #clientes.insert(nombre: 'ernesto')
 #clientes.insert(nombre: 'rony')
 
-#transacciones.insert(monto: -1000, fecha: '22-10-18')
-
-[   {:monto => 10, :fecha => "10-10-22"},                                                                  
-    {:monto => 20, :fecha => "12-10-22"},                                                                  
-    {:monto => 30, :fecha => "14-10-22"},
-    {:monto => 100, :fecha => "20-10-22"},
-    {:monto => -1000, :fecha => "30-10-22"},
-]  
+#transacciones.insert(monto: 1000, fecha: '22-10-18', nombre_cliente: "oscar")
+#transacciones.insert(monto: 1000, fecha: '22-10-18', nombre_cliente: "oscar")
+#transacciones.insert(monto: 1000, fecha: '22-10-18', nombre_cliente: "sofia")
+#transacciones.insert(monto: 1000, fecha: '22-10-18', nombre_cliente: "ernesto")
+#transacciones.insert(monto: 1000, fecha: '22-10-18', nombre_cliente: "ernesto")
