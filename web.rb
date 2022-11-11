4ecfa2857d50caf7858bced5b9c0469850a0528d
Oscar Vazquez <0skr.vazquez@gmail.com>
2022-11-11 12:24:16 -0600

    Refactorizando y cambiando las rutas

--- web.rb
+++ web.rb
@@ -1,13 +1,26 @@
 require 'sinatra'
+require "sequel"
 
+# Conexion base de datos
+DB = Sequel.connect('sqlite://cartera.db')
+
+# Routes
 get '/' do
-  erb :index
+  redirect '/clientes'
 end
 
-get '/:nombre' do |n|
-  erb :cliente, :locals => {:nombre => n}
+get '/clientes' do
+  c = DB[:clientes].all
+  erb :clientes, :locals => {:clientes => c}
 end
 
-post '/:nombre' do |n|
+
+get '/clientes/:nombre' do |n|
+  t = DB[:transacciones].where(nombre_cliente: n).all
+  erb :cliente, :locals => {:nombre => n, :transacciones => t}
+end
+
+post '/cliente' do |n|
   erb :nuevo_cliente, :locals => {:nombre => n}
+  
 end
